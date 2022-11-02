69eb0dd5bdbe9764a8164b9c55b4de872f785d9e
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-11-02 23:47:07 +0700

    Update atlas.config.js

--- provider/atlas.config.js
+++ provider/atlas.config.js
@@ -7,26 +7,13 @@ const ATLAS = {
 }
 
 const ATLAS_DB = {
-  wabotUsers: "db_user_wabot",
-  textCloud: "text_cloud",
-  imageCloud: "image_cloud",
-  list: {
-    toDo: "to_do",
-    absence: "absen",
-    link: "link"
-  }
+  azusaDatabase: 'AzusaBot'
 }
 
 const ATLAS_COLLECTION = {
-  clientWabot: "users",
-  azusaUsers: "users_azusabot",
-  text: "text_store",
-  image: "image_store",
-  _list: {
-    _toDo: "to_do_list",
-    _absence: "absen_list",
-    _link: "link_list"
-  }
+  azusaUsers: 'users',
+  azusaSticker: 'sticker',
+  azusaTotalcmd: 'totalcmd'
 }
 
 module.exports = { ATLAS, ATLAS_DB, ATLAS_COLLECTION }
