82419462277d7dcc089eda7275971a5a3478f57c
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-12-17 13:34:18 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,20 +1,25 @@
+/**
+ * @global
+ * @settings
+ */
+
 const fs = require('fs')
 const chalk = require('chalk')
-const  { indonesia, english, spanyol } = require(`./language`)
+const { indonesia, english, spanyol } = require(`./language`)
 
 // Website Api
 global.APIs = {
-	alfa: 'https://api.zeeoneofc.xyz'
+  alfa: 'https://api.zeeoneofc.xyz'
 }
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'nQw3DWLu',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+  'https://api.zeeoneofc.xyz': 'nQw3DWLu',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
 global.autoread = true // auto read message
-global.autobio = false 
+global.autobio = false
 global.userRegister = false
 
 //language
@@ -26,28 +31,28 @@ global.language = indonesia  //change indonesia to english if you don't understa
 // Other
 // Other
 global.botname = "Azusa Bot"
-global.ownername= "Azusa Chan"
-global.myweb ="Soon!..." 
+global.ownername = "Azusa Chan"
+global.myweb = "Soon!..."
 global.youtube = "wa.me/6281329585825" //change to chat owner
-global.myweb2 = "Soon!..." 
-global.email = "rizzzky78@gmail.com" 
-global.region = "Indonesia" 
+global.myweb2 = "Soon!..."
+global.email = "rizzzky78@gmail.com"
+global.region = "Indonesia"
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['6281329585825'] //premium user
 global.owner = ["6281329585825"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281329585825" 
-global.ownernomerr = "+6281329585825" 
+global.ownernomer = "6281329585825"
+global.ownernomerr = "+6281329585825"
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = 'Made by: ©AzusaBot' 
-global.author = 'wa.me/628988054766'
-global.sessionName = 'session'
+global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.packname = '©ShirasuAzusa-Bot'
+global.author = 'wa.me/6287777281751'
+global.sessionName = 'devsession' // session
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
-    premium: "Infinity",
-    free: 30
+  premium: "Infinity",
+  free: 100
 }
 
 global.adventureRPG = 'https://telegra.ph/file/30bef343159005b622ced.jpg'
@@ -70,26 +75,26 @@ global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutpu
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+  sukses: '🤗Done, Oke Desu~',
+  admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+  botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+  owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+  group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+  private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+  bot: 'Fitur Khusus Pengguna Nomor Bot',
+  errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+  wait: '⏳ Sedang Di Proses',
+  lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+  example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
+  fs.unwatchFile(file)
+  console.log(chalk.redBright(`Update'${__filename}'`))
+  delete require.cache[file]
+  require(file)
 })
