3f127ce8b16f21dd6b4b71d487cb7008e61ed64d
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-12-17 13:35:16 +0700

    Add files via upload

---
+++ services/jest.js
@@ -0,0 +1,194 @@
+
+const sections = [
+  {
+    title: "Kiana Kaslana",
+    rows: [
+      {
+        title: 'Herrscher of Void',
+        rowId: "er hov"
+      },
+      {
+        title: 'Herrscher of Flamescion',
+        rowId: "er hof"
+      },
+    ]
+  },
+  {
+    title: "Raiden Mei (sipaling beban)",
+    rows: [
+      {
+        title: 'Herrscher of Thunder (Basic Attack)',
+        rowId: "er hot"
+      },
+      {
+        title: 'Herrscher of Thunder (Burst)',
+        rowId: "er hotburst"
+      },
+      {
+        title: 'Valkyrie Bladestrike',
+        rowId: "er sf"
+      },
+    ]
+  },
+  {
+    title: "Bronya Zaychik",
+    rows: [
+      {
+        title: 'Herrscher of Reason',
+        rowId: "er hor"
+      },
+      {
+        title: 'SILVERWING: N-EX',
+        rowId: "er sw"
+      }
+    ]
+  },
+  {
+    title: "Kallen Kaslana",
+    rows: [
+      {
+        title: 'Ritual Imayoh',
+        rowId: "er ri"
+      }
+    ]
+  },
+  {
+    title: "Theresa Apocalypse (Loli Baba)",
+    rows: [
+      {
+        title: 'Twilight Paladin',
+        rowId: "er tp"
+      },
+      {
+        title: 'Luna Kindred',
+        rowId: "er lk"
+      }
+    ]
+  },
+  {
+    title: "Durandal : Bianka Ataegina",
+    rows: [
+      {
+        title: 'Palatinus Equinox',
+        rowId: "er pe"
+      },
+      {
+        title: 'Dea Anchora',
+        rowId: "er dea"
+      },
+      {
+        title: 'Bright Knight: Excelsisn',
+        rowId: "er bke"
+      },
+      {
+        title: 'Valkyrie Gloria',
+        rowId: "er vg"
+      },
+    ]
+  },
+  {
+    title: "Rita (babu)",
+    rows: [
+      {
+        title: 'Spina Astera',
+        rowId: "er ritasa"
+      },
+      {
+        title: 'Argent Knight: Artemis',
+        rowId: "er aka"
+      },
+      {
+        title: 'Fallen Rosemary',
+        rowId: "er fr"
+      },
+    ]
+  },
+  {
+    title: "Rozaliya",
+    rows: [
+      {
+        title: 'Fervent Tempo',
+        rowId: "er ft"
+      }
+    ]
+  },
+  {
+    title: "Seele Vollrei (sipaling lont)",
+    rows: [
+      {
+        title: 'Starchasm Nyx',
+        rowId: "er nyx"
+      },
+      {
+        title: 'Stygian Nymph',
+        rowId: "er sn"
+      },
+    ]
+  },
+  {
+    title: "Flame Chaser",
+    rows: [
+      {
+        title: 'Miss Pink Elf♪',
+        rowId: "er ely"
+      },
+      {
+        title: 'Herrscher of Human : Ego',
+        rowId: "er hoh"
+      },
+      {
+        title: 'Infinite Ouroboros',
+        rowId: "er io"
+      },
+      {
+        title: 'Pardofelis',
+        rowId: "er felis"
+      },
+      {
+        title: 'Aphonia',
+        rowId: "er aphonia"
+      },
+      {
+        title: 'Eden',
+        rowId: "er eden"
+      },
+      {
+        title: 'Griseo',
+        rowId: "er griseo"
+      }
+    ]
+  },
+  {
+    title: "Other",
+    rows: [
+      {
+        title: 'Herrscher of Sentience',
+        rowId: "er hos"
+      },
+      {
+        title: 'Prinzessin der Verurteilung',
+        rowId: "er faisal"
+      },
+      {
+        title: 'Midnight Absinthe',
+        rowId: "er ma"
+      },
+      {
+        title: 'Sweet n Spicy',
+        rowId: "er snc"
+      },
+      {
+        title: 'Kubis (AI CHWAN)',
+        rowId: "er kubis"
+      },
+      {
+        title: 'Jade Knight (Li Sushang)',
+        rowId: "er sus"
+      },
+      {
+        title: 'Vermilion Knight (si mayad)',
+        rowId: "er veki"
+      }
+    ]
+  }
+]
