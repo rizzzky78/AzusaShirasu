27c00be62390f5253592356511489622f3f26ceb
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-12-21 22:25:20 +0700

    Update global.config.js

--- config/global.config.js
+++ config/global.config.js
@@ -3,12 +3,35 @@
     Global Configuration Variables
 
 */
+let marks = '```'
+let infos = `
+${marks}
+Shirasu Azusa @WhatsAppBot
 
+Version: v4.0
+Deploy: 22 Dec 2022
+
+Server: VPS Client
+Monitored: PM2 IO
+AutoRestart: Yes
+
+
+Open Group Azusa Bot:
+https://chat.whatsapp.com/GEbLQ62KoWRFZcbRBrQYvd
+${marks}
+`
+/**
+ * @versionReleases
+ */
+const versioningBot = {
+  globalGroup: 'https://chat.whatsapp.com/GEbLQ62KoWRFZcbRBrQYvd',
+  infos
+}
 /**
  * @newRegister `for who has new Register`
  * @limitAwal `value as Number`
  */
-const makeLimitAwal = 40
+const makeLimitAwal = 50
 
 /**
  * @apikeys
@@ -16,7 +39,25 @@ const makeLimitAwal = 40
  */
 const MyApikeys = {
   LoLHumanApikeys: "rizzzuchi78apikey",
-
+  OpenAi: "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"
+};
+/**
+ * @interface
+ * @menu
+ */
+const interfaces = {
+  upper: '┏━',
+  side: '┃',
+  lower: '┗━',
+  marker: '⌬'
+};
+let picture = [
+  'https://raw.githubusercontent.com/rizzzky78/rizzzkyRepo/main/picture/img-azusa-main.jpeg',
+  'https://raw.githubusercontent.com/rizzzky78/rizzzkyRepo/main/picture/img-azusa-secondary.jpeg',
+  'https://raw.githubusercontent.com/rizzzky78/rizzzkyRepo/main/picture/img-azusa-third.jpeg'
+]
+const imgPlaceholder = {
+  azusa: picture[Math.floor(Math.random() * (picture.length))]
 }
 /**
  * MongoDB Atlas
@@ -26,14 +67,14 @@ const MyApikeys = {
 const ATLAS = {
   user: 'rizzzky',
   key: 'PRODS',
-}
+};
 /**
  * @Database
  * Lists of all databases from mongodb atlas cloud
  */
 const ATLAS_DB = {
   azusaDatabase: 'AzusaBot'
-}
+};
 /**
  * @collections
  * Lists of all colections from mongodb atlas cloud
@@ -44,6 +85,48 @@ const ATLAS_COLLECTION = {
   azusaTotalcmd: 'totalcmd',
   azusaCloud: 'azusa-cloud-data',
   azusaCloudUserStorage: 'user-data-store'
-}
+};
+/**
+ * @openAi
+ * @settings
+ */
+const OpenAiConfig = {
+  model: {
+    davinci: "text-davinci-003",
+    curie: "text-curie-001",
+    babbage: "text-babbage-001",
+    ada: "text-ada-001"
+  },
+  temperature: {
+    zero: 0.1,
+    first: 0.5,
+    second: 1,
+    third: 1.5
+  },
+  tokens: {
+    low: 100,
+    mid: 300,
+    high: 500,
+    expert: 2000
+  },
+  freqPenalty: {
+    optOne: 0.5,
+    optTwo: 1
+  },
+  prePenalty: {
+    optOne: 0.5,
+    optTwo: 1
+  }
+};
 
-module.exports = { MyApikeys, ATLAS, ATLAS_DB, ATLAS_COLLECTION, makeLimitAwal }
+module.exports = {
+  versioningBot,
+  MyApikeys,
+  ATLAS,
+  ATLAS_DB,
+  ATLAS_COLLECTION,
+  OpenAiConfig,
+  makeLimitAwal,
+  interfaces,
+  imgPlaceholder
+}
