d6143e1b3c5f647c962b6db67272a315db90f90b
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-12-22 21:23:35 +0700

    Update global.config.js

--- config/global.config.js
+++ config/global.config.js
@@ -18,6 +18,7 @@ AutoRestart: Yes
 
 Open Group Azusa Bot:
 https://chat.whatsapp.com/GEbLQ62KoWRFZcbRBrQYvd
+anyone can join & discuss
 ${marks}
 `
 /**
@@ -39,7 +40,7 @@ const makeLimitAwal = 50
  */
 const MyApikeys = {
   LoLHumanApikeys: "rizzzuchi78apikey",
-  OpenAi: "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"
+  OpenAi: "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"
 };
 /**
  * @interface
@@ -97,24 +98,24 @@ const OpenAiConfig = {
     ada: "text-ada-001"
   },
   temperature: {
-    zero: 0.1,
+    zero: 0.3,
     first: 0.5,
-    second: 1,
-    third: 1.5
+    second: 0.7,
+    third: 1
   },
   tokens: {
-    low: 100,
-    mid: 300,
-    high: 500,
-    expert: 2000
+    low: 500,
+    mid: 700,
+    high: 1800,
+    expert: 4000
   },
   freqPenalty: {
-    optOne: 0.5,
-    optTwo: 1
+    optOne: 0.7,
+    optTwo: 0.2
   },
   prePenalty: {
-    optOne: 0.5,
-    optTwo: 1
+    optOne: 0.8,
+    optTwo: 1.5
   }
 };
 
