c40eabe688236e793bcd273bb68e5f5179bd1fa6
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-12-26 18:28:28 +0700

    Update MenuMaker.js

--- interface/MenuMaker.js
+++ interface/MenuMaker.js
@@ -4,257 +4,257 @@
  */
 const SelectorMenu = [
   {
-    title: 'Azusa Bot Menu\'s',
+    title: '📑 Shirasu Azusa Bot Menu\'s',
     rows: [
       {
-        title: 'All Menu\'s',
+        title: '📒 All Menu\'s',
         rowId: 'semuamenu',
         description: 'Menampilkan semua Fitur Bot'
       },
       {
-        title: 'Info Bot',
+        title: '📔 Info Bot',
         rowId: 'infobot',
-        description: 'Menampilkan informasi tentang Bot'
+        description: 'Menampilkan informasi tentang Bot & Grup Bot'
       },
       {
-        title: 'Support Owner',
-        rowId: 'donasi',
-        description: 'Dukung owner agar Bot tetap hidup'
+        title: '📗 Panduan',
+        rowId: 'guide',
+        description: 'Bingung, butuh bantuan cara pakai Bot?, baca tutorial disini.'
       },
       {
-        title: 'Tambah Limit',
+        title: '📕 Cloud Store',
+        rowId: 'storehelp',
+        description: 'Cara pakai Cloud Store'
+      },
+      {
+        title: '📘 Rules',
+        rowId: 'rulesbot',
+        description: 'Peraturan terkait penggunaan Bot'
+      },
+      {
+        title: '📙 FAQ',
+        rowId: 'faq',
+        description: 'Pertanyaan seputar Bot yang mungkin sama'
+      },
+      {
+        title: '💷 Tambah Limit',
         rowId: 'howtolimit',
         description: 'Buy limit agar tetap bisa menggunakan fitur Bot'
+      },
+      {
+        title: '💰 Support Owner',
+        rowId: 'donasi',
+        description: 'Dukung owner agar Bot tetap hidup'
       }
     ]
   },
   {
-    title: 'Handler Menu',
+    title: '📑 Handler Menu',
     rows: [
       {
-        title: 'Owner Menu',
+        title: '🗂️ Owner Menu',
         rowId: 'azusacommand ownermenu',
         description: 'Fitur khusus Owner Bot'
       },
       {
-        title: 'Cloud Store',
+        title: '📁 Cloud Store',
         rowId: 'azusacommand cloudstore',
         description: 'Menyimpan sesuatu ke Database Bot'
       },
       {
-        title: 'Open Ai',
+        title: '🤖 Open Ai',
         rowId: 'openai',
         description: 'Fitur Exclusive, direspon dengan Ai / kecerdasan buatan'
       },
       {
-        title: 'Group & Management',
+        title: '👤 Group & Management',
         rowId: 'azusacommand groupmenu',
         description: 'Fitur khusus Grup beserta manajemen Grup'
       },
     ]
   },
   {
-    title: 'Users Menu',
+    title: '📑 Users Menu',
     rows: [
       {
-        title: 'Anonymous Chat',
+        title: '🪪 My Profile',
+        rowId: 'myprofile',
+        description: 'Menampilkan status dan profile kamu'
+      },
+      {
+        title: '👤👤 Anonymous Chat',
         rowId: 'azusacommand anonymous',
         description: 'Fitur untuk melakukan chat random'
       },
       {
-        title: 'RGP Games',
+        title: '🎮 RGP Games',
         rowId: 'azusacommand rpggames',
         description: 'Fitur RPG Adventure Games mirip seperti Growtopia'
       },
       {
-        title: 'Quiz',
+        title: '🧩 Quiz',
         rowId: 'azusacommand quizmenu',
         description: 'Random Quiz game'
       }
     ]
   },
   {
-    title: 'Game Guides n Game FAQ',
+    title: '📑 Game Guides n Game FAQ',
     rows: [
       {
-        title: 'Honkai Impact ER Recomended Signet',
+        title: '📚 Honkai Impact ER Recomended Signet',
         rowId: 'signet',
         description: 'v6.2 Rekomendasi signet ER - fase 1'
       },
       {
-        title: 'Genshin Impact Material Ascend',
+        title: '📚 Genshin Impact Material Ascend',
         rowId: 'genshinmaterial',
         description: 'Lihat kebutuhan material untuk built-up karakter'
       },
       {
-        title: 'Genshin Impact Build',
+        title: '📚 Genshin Impact Build',
         rowId: 'genshinbuild',
         description: 'Lihat rekomendasi build karakter'
       }
     ]
   },
   {
-    title: 'Get Features',
+    title: '🧰 Tools',
     rows: [
       {
-        title: 'Downloader',
+        title: '📥 Downloader',
         rowId: 'azusacommand downloadermenu',
         description: 'Mendowload sesuatu via Bot'
       },
       {
-        title: 'Searcher',
+        title: '🔍 Searcher',
         rowId: 'azusacommand searchmenu',
         description: 'Mencari sesuatu yang gak penting amat'
       },
       {
-        title: 'Islami',
+        title: '🕋 Islami',
         rowId: 'azusacommand islamicmenu',
         description: 'Stay halal brother :))'
       }
     ]
   },
   {
-    title: 'Otaku\'s',
+    title: '⚗️ Otaku\'s',
     rows: [
       {
-        title: 'Animanga',
+        title: '🔖 Animanga',
         rowId: 'azusacommand animanga',
         description: 'Segala sesuatu tentang Anime & Manga'
       },
       {
-        title: 'Anime Picture & Wallpaper',
+        title: '🔖 Anime Picture & Wallpaper',
         rowId: 'azusacommand animepicture',
         description: 'Kumpulan wallpaper anime'
       },
       {
-        title: 'NSFW Menu',
+        title: '🔖 NSFW Menu',
         rowId: 'azusacommand nsfwmenu',
         description: 'Not Safe For Work (+18) Menu'
       },
       {
-        title: 'Lewd Picture',
+        title: '🔖 Lewd Picture',
         rowId: 'azusacommand lewdmenu',
         description: 'Kumpulan Gambar Lewd (+18) Menu'
       }
     ]
   },
   {
-    title: 'Entertainment, Meme & Tele Sticker',
+    title: '📱 Entertainment, Meme & Tele Sticker',
     rows: [
       {
-        title: 'Meme\'s',
+        title: '💈 Meme\'s',
         rowId: 'azusacommand memes',
         description: 'Kumpulan memes & meme creator'
       },
       {
-        title: 'Movie',
+        title: '💈 Movie',
         rowId: 'azusacommand movie',
         description: 'Melihat info seputar perfilman'
       },
       {
-        title: 'Story',
+        title: '💈 Story',
         rowId: 'azusacommand story',
         description: 'Fitur pembuat cerita fiktif'
       },
       {
-        title: 'Quotes',
+        title: '💈 Quotes',
         rowId: 'azusacommand quotes',
         description: 'Fitur random quotes'
       },
       {
-        title: 'Asupan',
+        title: '💈 Asupan',
         rowId: 'azusacommand asupan',
         description: 'Bahan penyegar dan cuci mata :v'
       },
       {
-        title: 'Random Cewe',
+        title: '💈 Random Cewe',
         rowId: 'azusacommand randomcewe',
         description: 'Kumpulan gambar random cewe cantik'
       },
       {
-        title: 'Telegram Sticker',
+        title: '💈 Telegram Sticker',
         rowId: 'azusacommand telesticker',
         description: 'Kumpulan sticker Telegram'
       }
     ]
   },
   {
-    title: 'Editor Menu',
+    title: '🎨 Editor Menu',
     rows: [
       {
-        title: 'Converter',
+        title: '📇 Converter',
         rowId: 'azusacommand convertermenu',
         description: 'Mengkonversi sesuatu dnegan Bot'
       },
       {
-        title: 'Image Effect',
+        title: '🖼️ Image Effect',
         rowId: 'azusacommand imageeffect',
         description: 'Membuat gambar jadi lebih menarik'
       },
       {
-        title: 'Sticker Effect',
+        title: '🖼️ Sticker Effect',
         rowId: 'azusacommand stickeffect',
         description: 'Membuat sticker jadi lebih menarik'
       },
       {
-        title: 'Primbon',
+        title: '📼 Primbon',
         rowId: 'azusacommand primbonmenu',
         description: 'Primbon Efek'
       },
       {
-        title: 'Ephoto360',
+        title: '📷 Ephoto360',
         rowId: 'azusacommand ephoto',
         description: 'Mengedit sesuatu dengan Ephoto360 via Bot'
       },
       {
-        title: 'Text Pro Maker',
+        title: '🖊️ Text Pro Maker',
         rowId: 'azusacommand textpro',
         description: 'Membuat watermark keren dengan Bot'
       },
       {
-        title: 'Logo Maker',
+        title: '✒️ Logo Maker',
         rowId: 'azusacommand logomaker',
         description: 'Membuat Logo keren dengan Bot'
       },
       {
-        title: 'Voice Changer',
+        title: '🎙️ Voice Changer',
         rowId: 'azusacommand voicechanger',
         description: 'Filter audio agar menjadi lebih menarik'
       }
     ]
   },
   {
-    title: 'Other Features',
+    title: '📓 Other Features',
     rows: [
       {
         title: 'Others',
         rowId: 'azusacommand other',
         description: 'Menampilkan fitur lainnya'
-      }
-    ]
-  },
-  {
-    title: 'Help / Bantuan & Rules & FAQ',
-    rows: [
-      {
-        title: 'Panduan',
-        rowId: 'guide',
-        description: 'Bingung, butuh bantuan?, baca tutorial disini.'
-      },
-      {
-        title: 'Cloud Store',
-        rowId: 'storehelp',
-        description: 'Cara pakai Cloud Store'
-      },
-      {
-        title: 'Rules',
-        rowId: 'rulesbot',
-        description: 'Peraturan terkait penggunaan Bot'
-      },
-      {
-        title: 'FAQ',
-        rowId: 'faq',
-        description: 'Pertanyaan yang mungkin sama'
       }
     ]
   }
